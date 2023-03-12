2a8bbe50d91800893a03e23c76ff8c70a6d12399
sifuraz <martin@borner.dev>
2023-03-12 11:29:38 +0100

    feat: setup fastapi

--- app/main.py
+++ app/main.py
@@ -1,21 +1,12 @@
-from db.setup import session
-from db.models.user import User
+from fastapi import FastAPI
 
-# This is a sample Python script.
+from .db.setup import session
+from .db.models.user import User
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-# TODO uvicorn setup
+app = FastAPI()
 
 
-def print_hi():
+@app.get("/")
+async def root():
     user: User = session.query(User).filter_by(username="test").first()
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {user.email}")  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    print_hi()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return {"message": f"Hi, {user.email}"}
